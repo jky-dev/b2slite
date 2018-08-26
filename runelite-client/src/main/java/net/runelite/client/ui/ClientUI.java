99aa62a5e60036466300cc6380e6305608e0a520
Jacky <liangj97@gmail.com>
2018-08-26 10:58:54 +1000

    Change focus calls

--- runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
+++ runelite-client/src/main/java/net/runelite/client/ui/ClientUI.java
@@ -512,19 +512,14 @@ public class ClientUI
 		{
 			OSXUtil.requestFocus();
 		}
+		else if (OSType.getOSType() == OSType.Windows && System.getProperty("os.name").contains("10"))
+		{
+			frame.toFront();
+		}
 
-		if (OSType.getOSType() == OSType.Windows && System.getProperty("os.name").contains("10"))
-		{
-			frame.setAlwaysOnTop(true);
-			frame.requestFocus();
-			giveClientFocus();
-			frame.setAlwaysOnTop(config.gameAlwaysOnTop());
-		}
-		else
-		{
-			frame.requestFocus();
-			giveClientFocus();
-		}
+		frame.requestFocus();
+		giveClientFocus();
+
 	}
 
 	/**
