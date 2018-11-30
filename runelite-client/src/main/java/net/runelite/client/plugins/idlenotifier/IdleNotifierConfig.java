f4b789ec0a55bc10005240c66a70ac07e8f1b8d9
Jacky <liangj97@gmail.com>
2018-11-30 19:41:00 +1100

    add back notifier settings

--- runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleNotifierConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/idlenotifier/IdleNotifierConfig.java
@@ -107,4 +107,36 @@ public interface IdleNotifierConfig extends Config
 	{
 		return 0;
 	}
+
+	@ConfigItem(
+		keyName = "moving",
+		name = "Idle Moving",
+		position = 20,
+		description = "If no animation and not moving"
+	)
+	default boolean idlemoving()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "mapTiles",
+		name = "Map Tiles",
+		position = 21,
+		description = "Valid map tiles for idlemoving"
+	)
+	default String mapTiles()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+		keyName = "fishingidle",
+		name = "Idle Fishing Notifications",
+		description = "Configures if idle fishing notifications are enabled"
+	)
+	default boolean fishingIdle()
+	{
+		return true;
+	}
 }
