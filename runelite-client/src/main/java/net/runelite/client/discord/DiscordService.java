e3023f0ee0f247bf0493a29249f748857b4c01c7
Jacky <liangj97@gmail.com>
2019-05-16 19:19:03 +1000

    Add custom application for discord

--- runelite-client/src/main/java/net/runelite/client/discord/DiscordService.java
+++ runelite-client/src/main/java/net/runelite/client/discord/DiscordService.java
@@ -106,7 +106,8 @@ public class DiscordService implements AutoCloseable
 		discordEventHandlers.joinGame = this::joinGame;
 		discordEventHandlers.spectateGame = this::spectateGame;
 		discordEventHandlers.joinRequest = this::joinRequest;
-		discordRPC.Discord_Initialize(runeLiteProperties.getDiscordAppId(), discordEventHandlers, true, null);
+		//discordRPC.Discord_Initialize(runeLiteProperties.getDiscordAppId(), discordEventHandlers, true, null);
+		discordRPC.Discord_Initialize("577366954772529172", discordEventHandlers, true, null);
 		executorService.scheduleAtFixedRate(discordRPC::Discord_RunCallbacks, 0, 2, TimeUnit.SECONDS);
 	}
 
