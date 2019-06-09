94c89a4888bc44f540c663472022dbfc13d69039
Jacky <liangj97@gmail.com>
2019-06-09 13:05:07 +1000

    Add teleport to poh menu swap

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -570,6 +570,7 @@ public class MenuEntrySwapperPlugin extends Plugin
 		// Put all item-related swapping after shift-click
 		else if (config.swapTeleportItem() && (option.equals("wear") || option.equals("remove")))
 		{
+			swap("teleport to poh", option, target, true);
 			swap("teleport", option, target, true);
 			swap("xeric's glade", option, target, true);
 			swap("farm teleport", option, target, true);
