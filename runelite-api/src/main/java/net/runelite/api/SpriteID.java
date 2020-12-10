0cfd59837a6b7732b1fa4e434fb5b74d09dd715a
jkybtw <liangj97@gmail.com>
2020-12-11 09:38:26 +1100

    Add hydra sprite ids back

--- runelite-api/src/main/java/net/runelite/api/SpriteID.java
+++ runelite-api/src/main/java/net/runelite/api/SpriteID.java
@@ -1563,6 +1563,9 @@ public final class SpriteID
 	public static final int TAB_MAGIC_SPELLBOOK_ARCEUUS_UNUSED = 1708;
 	/* Unmapped: 1709, 1710 */
 	public static final int TAB_MAGIC_SPELLBOOK_ARCEUUS = 1711;
+	public static final int BIG_ASS_GUTHIX_SPELL = 1774;
+	public static final int BIG_SUPERHEAT = 1800;
+	public static final int BIG_SPEC_TRANSFER = 1959;
 	/* Unmapped: 1712~2175 */
 	public static final int HEALTHBAR_DEFAULT_FRONT_30PX = 2176;
 	public static final int HEALTHBAR_DEFAULT_BACK_30PX = 2177;
